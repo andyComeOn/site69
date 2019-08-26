d8defc4a4242f509a788bd2028fdb45449e7e33a
AndyHasee <1174839512@qq.com>
2019-08-26 23:45:12 +0800

    test cookie logic

---
+++ demophp/getcookie.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="" href="http://139.199.23.2/img/favicon/study.png" />
+    <title>快速测试php</title>
+
+</head>
+
+<body>
+    <?php
+        
+        print_r($_COOKIE);
+        // 2019-09-02T15:32:19.502Z
+    ?>
+</body>
+</html>
---
+++ demophp/setcookie.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="" href="http://139.199.23.2/img/favicon/study.png" />
+    <title>快速测试php</title>
+
+</head>
+
+<body>
+    <?php
+        setcookie('username','admin',time()+7*24*3600);
+        // print_r("年后$v");
+    ?>
+</body>
+</html>
