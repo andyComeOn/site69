d89242f69dafb82bc33e4d7ad5dd1b6a861a6a9e
zhangboo <1174839512@qq.com>
2019-08-27 11:48:16 +0800

    add fast1.php time.php

--- demophp/fast1.php
+++ demophp/fast1.php
@@ -3,18 +3,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <!-- <meta charset="utf-8"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" type="" href="http://139.199.23.2/img/favicon/study.png" />
-    <title>php时间输出</title>
+    <title>快速测试php</title>
 
 </head>
 
 <body>
     <?php
-        
-        echo '<br>',date("Y/m/d H:i:s");
+        echo "<img src='http://139.199.23.2/img/favicon/study.png'/>";
+       
     ?>
 </body>
 </html>
