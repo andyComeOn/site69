54f4561bbe80a2c81acc6f0f7d4eb6c88992b708
AndyHasee <1174839512@qq.com>
2019-09-11 21:43:52 +0800

    提交神州测试文件

---
+++ demophp/array.php
@@ -0,0 +1,23 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="" href="http://139.199.23.2/img/favicon/study.png" />
+    <title>php数组</title>
+
+</head>
+
+<body>
+    <?php
+       
+        $arrs = array('andy', 'jim', 'tom');
+        foreach($arrs as $v) {
+            echo $v . '<br>';
+        }
+    ?>
+</body>
+</html>
