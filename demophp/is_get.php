a534f15afef4d816924f1453ad651cda61672291
zhangboo <1174839512@qq.com>
2019-10-08 11:16:43 +0800

    添加blog15

---
+++ demophp/is_get.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="" href="http://139.199.23.2/img/favicon/study.png" />
+    <title>快速测试php</title>
+
+</head>
+
+<body>
+    <?php
+    echo '快速测试PHP，Hello World<br>';
+    // print_r();
+    if (isset($_GET['a'])) {
+        echo $_GET['a'];
+    }
+    // isset($_GET['a']);
+    
+    
+   
+
+
+
+    
+    ?>
+</body>
+
+</html>
